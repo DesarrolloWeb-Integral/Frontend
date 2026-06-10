b07b8cf4314d755710afbf5feefbfe6c3824d5c9
IsaacCanoH <isac.c.2586@gmail.com>
2026-06-09 20:22:39 -0600

    fix: probar despliegue con nueva cuenta de vercel

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <main>
       <h1>Frontend CRM Agrícola</h1>
 
-      <h2>Test 2</h2>
+      <h2>Test 1</h2>
 
       <p>Proyecto frontend creado con React y TypeScript.</p>
     </main>
